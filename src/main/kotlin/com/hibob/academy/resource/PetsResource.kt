5a223008cb63c25e394340ec529a50af25959af0
yechezkel.nikop <yechezkelnikop@gmail.com>
2024-09-14 21:37:33 +0300

    change to Long

--- src/main/kotlin/com/hibob/academy/resource/PetsResource.kt
+++ src/main/kotlin/com/hibob/academy/resource/PetsResource.kt
@@ -6,7 +6,7 @@ import jakarta.ws.rs.core.Response
 import org.springframework.stereotype.Controller
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.*
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 @Controller
 @Path("/api/pets")
@@ -30,7 +30,7 @@ class PetsResource {
     fun postPet(pet: Pet): Response {
         val newPetId = (allPets.maxByOrNull { p -> p.id }?.id ?: 0) + 1
 
-        allPets.add(pet.copy(id = newPetId, dateOfArrival = LocalDateTime.now()))
+        allPets.add(pet.copy(id = newPetId, dateOfArrival = LocalDate.now()))
 
         return Response.status(Response.Status.CREATED).entity(Response.Status.CREATED).build()
     }
@@ -40,7 +40,7 @@ class PetsResource {
     fun putPet(@PathParam("petId") petId: Long ,updatePet: Pet): Response {
         val index = allPets.indexOfFirst { p -> p.id == petId }
         if (index >= 0) {
-            val petToUpdate = allPets.removeAt(index).copy(id =updatePet.id, dateOfArrival = LocalDateTime.now())
+            val petToUpdate = allPets.removeAt(index).copy(id =updatePet.id, dateOfArrival = LocalDate.now())
             return Response.ok(petToUpdate).build()
         } else {
             return Response.status(Response.Status.NOT_FOUND).entity("Pet not found").build()
--- src/main/kotlin/com/hibob/rsetApiDataClasses/restApiDataClass.kt
+++ src/main/kotlin/com/hibob/rsetApiDataClasses/restApiDataClass.kt
@@ -1,7 +1,7 @@
 package com.hibob.rsetApiDataClasses
 
-import java.time.LocalDateTime
+import java.time.LocalDate
 
-data class Owner(val id: Long, val name: String, val companyId: Long, val employeeId: Long)
+data class Owner(val id: Long, val name: String, val companyId: Long, val employeeId: String)
 
-data class Pet(val id: Long, val name: String, val type: String, val companyId: Long, val dateOfArrival: LocalDateTime)
+data class Pet(val id: Long, val name: String, val type: String, val companyId: Long, val dateOfArrival: LocalDate)
