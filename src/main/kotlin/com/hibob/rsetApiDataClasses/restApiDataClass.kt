353cbd63e7750ab4fbf3dc87e52f6d84d3d7ccb7
yechezkel.nikop <yechezkelnikop@gmail.com>
2024-09-14 21:37:33 +0300

    change to Long

--- src/main/kotlin/com/hibob/rsetApiDataClasses/restApiDataClass.kt
+++ src/main/kotlin/com/hibob/rsetApiDataClasses/restApiDataClass.kt
@@ -2,6 +2,6 @@ package com.hibob.rsetApiDataClasses
 
 import java.time.LocalDate
 
-data class Owner(val id: Long, val name: String?, val firstName: String?, val lastName: String?, val companyId: Long, val employeeId: Long)
+data class Owner(val id: Long, val name: String?, val firstName: String?, val lastName: String?, val companyId: Long, val employeeId: String)
 
 data class Pet(val id: Long, val name: String, val type: String, val companyId: Long, val dateOfArrival: LocalDate)
