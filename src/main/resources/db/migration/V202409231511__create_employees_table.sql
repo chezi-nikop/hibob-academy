dee29073c9388541d73ac90372c407f93c3d712d
yechezkel.nikop <yechezkelnikop@gmail.com>
2024-09-23 22:24:35 +0300

    delete check from role and added if not exists

--- src/main/resources/db/migration/V202409231511__create_employees_table.sql
+++ src/main/resources/db/migration/V202409231511__create_employees_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE employees
+CREATE TABLE IF NOT EXISTS employees
 (
     id         SERIAL PRIMARY KEY,
-    first_name VARCHAR(255)                                                 NOT NULL,
-    last_name  VARCHAR(255)                                                 NOT NULL,
-    role       VARCHAR(50) CHECK (role IN ('admin', 'manager', 'employee')) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    role       VARCHAR(50)  NOT NULL,
     company_id INT REFERENCES company (id)
 );
--- src/main/resources/db/migration/V202409231550__remove_check_from_employees_table.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE employees
-DROP COLUMN role;
-
-ALTER TABLE employees
-ADD COLUMN role VARCHAR(50) NOT NULL;
-
