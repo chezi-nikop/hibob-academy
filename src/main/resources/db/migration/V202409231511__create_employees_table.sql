a0f333cfc1e031955a2c48bef6ac6105ce9477e4
yechezkel.nikop <yechezkelnikop@gmail.com>
2024-09-24 09:51:56 +0300

    change BIGINT/BIGSERIAL and added index

--- src/main/resources/db/migration/V202409231511__create_employees_table.sql
+++ src/main/resources/db/migration/V202409231511__create_employees_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS employees
 (
-    id         SERIAL PRIMARY KEY,
+    id         BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
     role       VARCHAR(50)  NOT NULL,
-    company_id INT REFERENCES company (id)
+    company_id BIGINT REFERENCES company (id)
 );
+
+CREATE INDEX idx_company_id on employees (company_id);
