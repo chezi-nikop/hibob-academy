cef2f0793bcd99b2b2e19770c17d090ce3b2f29a
yechezkel.nikop <yechezkelnikop@gmail.com>
2024-09-25 14:50:47 +0300

    finish with resource for now

--- src/main/kotlin/com/hibob/academy/employeeFeedback/filter/AuthenticationFilter.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/filter/AuthenticationFilter.kt
@@ -11,14 +11,15 @@ import org.springframework.stereotype.Component
 
 @Component
 @Provider
-class AuthenticationFilter : ContainerRequestFilter {
+class AuthenticationFilterEmployee : ContainerRequestFilter {
     companion object {
-        private const val LOGIN_PATH = "/api/employees/login"
+        private const val LOGIN_PATH = "api/employees/login"
         const val COOKIE_NAME = "employee_cookie"
     }
 
     override fun filter(requestContext: ContainerRequestContext) {
-        if (requestContext.uriInfo.path == LOGIN_PATH) return //first login
+        //first login
+        if (requestContext.uriInfo.path == LOGIN_PATH) return
 
         //verify the JWT token from the cookie
         val cookie = requestContext.cookies[COOKIE_NAME]?.value //taking all the cookie from the req
@@ -34,7 +35,7 @@ class AuthenticationFilter : ContainerRequestFilter {
     //It uses the secret key SECRET_KEY in order to interpret the JWT. It can verify the validity and signature of the JWT.
     private val jwtParser = Jwts.parserBuilder().setSigningKey(SECRET_KEY).build()
 
-    private fun verify(cookie: String?, requestContext: ContainerRequestContext) : Claims? {
+    private fun verify(cookie: String?, requestContext: ContainerRequestContext): Claims? {
         if (cookie.isNullOrEmpty()) {
             //abortWith func used in case the authentication token is missing or incorrect please. returns HTTP 401
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Missing token").build())
@@ -44,7 +45,9 @@ class AuthenticationFilter : ContainerRequestFilter {
             // Parse the token and extract claims
             jwtParser.parseClaimsJws(cookie).body
         } catch (exception: Exception) {
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("Invalid or expired token").build())
+            requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED).entity("Invalid or expired token").build()
+            )
             null
         }
     }
--- src/main/kotlin/com/hibob/academy/employeeFeedback/resource/EmployeeResource.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/resource/EmployeeResource.kt
@@ -1,5 +1,6 @@
 package com.hibob.academy.employeeFeedback.resource
 
+import com.hibob.academy.employeeFeedback.dao.EmployeeDataForLogin
 import com.hibob.academy.employeeFeedback.service.EmployeeService
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.*
@@ -7,18 +8,19 @@ import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.NewCookie
 import jakarta.ws.rs.core.Response
 import org.springframework.stereotype.Controller
-//import com.hibob.academy.employeeFeedback.filter.AuthenticationFilter.COOKIE_NAME
+import com.hibob.academy.employeeFeedback.filter.AuthenticationFilterEmployee.Companion.COOKIE_NAME
 
 @Controller
 @Path("/api/employees")
 class EmployeeResource(private val employeeService: EmployeeService) {
+
     @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
-    fun login(employeeLogin: EmployeeLogin): Response {
+    fun login(employeeLogin: EmployeeDataForLogin): Response {
         val newToken = employeeService.loginEmployee(employeeLogin)
 
-        val newCookie = NewCookie.Builder(COOKIE_NAME).value(newToken).path("/api/").build()
+        val newCookie = NewCookie.Builder(COOKIE_NAME).value(newToken).path("/api").build()
 
         return Response.ok("login successfully").cookie(newCookie).build()
     }
