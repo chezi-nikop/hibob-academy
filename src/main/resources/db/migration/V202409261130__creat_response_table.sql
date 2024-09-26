05ae79608e0b2c4ce9730298415c79ed1c8dbc9a
yechezkel.nikop <yechezkelnikop@gmail.com>
2024-09-26 18:46:02 +0300

    added companyId

--- src/main/resources/db/migration/V202409261130__creat_response_table.sql
+++ src/main/resources/db/migration/V202409261130__creat_response_table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE if not exists response
 (
     id         BIGSERIAL PRIMARY KEY,
-    response_id BIGINT NOT NULL,
+    responder_id BIGINT NOT NULL,
     feedback_id BIGINT NOT NULL,
     content    TEXT   NOT NULL,
+    company_Id BIGINT NOT NULL,
     date       DATE DEFAULT CURRENT_DATE
 );
 
-CREATE INDEX idx_feedback_id on response (feedback_id);
+CREATE INDEX idx_feedback_id on response (feedback_id, company_Id);
