a0de67fd29bb6155c6bbd83c2ddfe6add3c26aba
chezi-nikop <yechezkelnikop@gmail.com>
2024-09-29 10:20:24 +0300

    Phase 2:  feedback service (#65)
    * in the middle of creating the filter func
    
    * finish with service
    
    * fixed update fun

--- src/main/kotlin/com/hibob/academy/employeeFeedback/service/FeddbackService.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/service/FeddbackService.kt
@@ -27,6 +27,17 @@ class FeedbackService(private val feedbackDao: FeedbackDao) {
         return returnedStatus
     }
 
+    fun updateFeedbackStatus(updateFeedback: UpdateStatus, companyId: Long): String {
+        validatePositiveIds(updateFeedback.feedbackId)
+        val updateStatus = feedbackDao.updateFeedbackStatus(updateFeedback, companyId)
+        if (updateStatus > 0) return "feedback status updated successfully"
+        return "feedback status has not been updated"
+    }
+
+    fun getFeedbackByFilter(filter: FeedbackFilter, companyId: Long): List<FeedbackDataOut> {
+        return feedbackDao.getFeedbackByFilter(filter, companyId)
+    }
+
     private fun validatePositiveIds(feedbackId: Long) {
         if (feedbackId <= 0) {
             throw BadRequestException("ID must be positive.")
